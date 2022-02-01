7503c27e00810b978baa9527eca0e3fba7f11e3d
DarianMD <darianmanasturean@gmail.com>
2022-02-01 20:08:47 +0100

    request funcionando

---
+++ app/Http/Controllers/UsrRegister.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+
+class UsrRegister extends Controller
+{
+    public function registro(Request $request){
+        return 'hh';
+    }
+}

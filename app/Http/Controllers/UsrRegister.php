8f57c6a44635c4cb6b60131ab3a1124fd036102a
DarianMD <darianmanasturean@gmail.com>
2022-02-02 13:47:03 +0100

    validates

--- app/Http/Controllers/UsrRegister.php
+++ app/Http/Controllers/UsrRegister.php
@@ -9,6 +9,22 @@ use Illuminate\Routing\Controller;
 class UsrRegister extends Controller
 {
     public function registro(Request $request){
-        return $request->input('name');
+
+        $validated = $request->validate([
+            'name' => 'max:4|required',
+            'email' => 'max:10|required',
+
+
+        ]);
+
+
+
+            if($validated){
+                return 'Correcto';
+            }
+            else{
+                return 'Error';
+            }
+
     }
 }

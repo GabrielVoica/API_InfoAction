381f42203c16cee9db7059c998c70af25b80c5b1
Gabriel Voica <gabi_voica@hotmail.com>
2022-03-02 19:20:26 +0100

    Changed origin for CORS to localhost

--- app/services/cookies/CookieService.php
+++ app/services/cookies/CookieService.php
@@ -24,7 +24,7 @@ class CookieService
 
     public function setCookie()
     {
-        setcookie($this->cookieName, $this->cookieValue, $this->expiresAt,'/','localhost');
+        setcookie($this->cookieName, $this->cookieValue,['expires' => $this->expiresAt,'secure' => false,'samesite' => 'Strict']);
     }
 
 
--- app/src/middleware/ProcessPetition.php
+++ app/src/middleware/ProcessPetition.php
@@ -13,9 +13,10 @@ class ProcessPeticion{
     * 
     */
    public function process(){
-      header('Access-Control-Allow-Origin: *');
+      header('Access-Control-Allow-Origin: http://localhost:4200');
       header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
       header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+      header('Access-Control-Allow-Credentials: true');
       header("Allow: GET, POST, OPTIONS, PUT, DELETE");
    }
 

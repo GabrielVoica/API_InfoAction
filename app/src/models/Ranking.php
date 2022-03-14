3002f0cdba4cf83363c79b21e0e3751b5aee57ca
DarianMD <darianmanasturean@gmail.com>
2022-03-14 18:52:52 +0100

    back

--- app/src/models/Ranking.php
+++ app/src/models/Ranking.php
@@ -1,9 +1,9 @@
 <?php
 
 require_once("services/Database.php");
-require_once("services/Validator.php");
-require_once("services/errors/NotFoundError.php");
-require_once("services/Insert.php");
+require_once("src/lib//Validator.php");
+require_once("src/lib/errors/NotFoundError.php");
+require_once("src/lib/Insert.php");
 
 
 class Ranking implements Model
@@ -31,8 +31,6 @@ class Ranking implements Model
     public static function insert(array $fields = null)
     {
 
-
-
         $columnsRequired = Insert::showRequiredColumns($fields['ranking_name']);
         $columnsAll = Insert::showColumnsWithoutID($fields['ranking_name']);
 
@@ -49,6 +47,7 @@ class Ranking implements Model
 
         }
 
+        
 
         $database = new Database();
         $database->connect();
--- app/src/models/RankingMembers.php
+++ app/src/models/RankingMembers.php
@@ -14,78 +14,16 @@ class RankingMembers implements Model
     public static function get($id = null, array $fields = null)
     {
 
-        if ($fields === null) {
-            $query = "SELECT * FROM user WHERE id = $id";
-        } else {
-            $query = "SELECT * FROM user WHERE ";
-            $keys = array_keys($fields);
-            $values = array_values($fields);
-            for ($i = 0; $i < count($fields); $i++) {
-                if (count($fields) === 1) {
-                    $query = $query . "$keys[$i] = '$values[$i]'";
-                } else {
-                    $query = $query . "$keys[$i] = '$values[$i]' AND ";
-                    if ($i == count($fields) - 1) {
-                        $query = substr($query, 0, strlen($query) - 5);
-                    }
-                }
-            }
-        }
-
-        $database = new Database();
-
-        $database->connect();
-
-        $data = $database->getConnection()->query($query);
-
-        if ($data === false) {
-            return false;
-        }
-
-        if (mysqli_num_rows($data) == 0) {
-            return false;
-        }
-
-        $data = mysqli_fetch_assoc($data);
-
-
-
-
-        return $data;
     }
-
-
-
-    public static function getField($id, $field)
-    {
-        $query = "SELECT $field FROM user WHERE id = $id";
-
-        $database = new Database();
-        $database->connect();
-        $data = $database->getConnection()->query($query);
-        $data = mysqli_fetch_assoc($data);
-
-        return $data[$field];
-    }
-
-
-
-   
 
     public static function getAll()
     {
-        $database = new Database();
-        $database->connect();
-        $data = $database->getConnection()->query('SELECT * FROM user');
-        $data = mysqli_fetch_assoc($data);
-
-        return $data;
+      
     }
 
 
     public static function insert(array $fields = null)
     {
-
         
         $columnsRequired = Insert::showRequiredColumns('user');
         $columnsAll = Insert::showColumnsWithoutID('user');
